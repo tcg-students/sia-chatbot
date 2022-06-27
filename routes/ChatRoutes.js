3d888e57f65edb433df8ce277ad0751f2b027dde
tumikole <samuelramollo@gmail.com>
2022-06-27 14:16:17 +0200

    added frontend folder and also added queries for getting nodes based on their id

--- routes/ChatRoutes.js
+++ routes/ChatRoutes.js
@@ -1,16 +1,23 @@
-const {getChatMessages} = require('../chatQueries/ChatQueries')
-
+const { getInitialTreeMessages,
+  getNodeMessages 
+  } = require("../chatQueries/ChatQueries");
 
 const chatRoutes = (app) => {
+  app.get("/allMessages", async (req, res) => {
+    const treeMessages = await getInitialTreeMessages();
+    console.log("first");
+    res.send({ message: "All tree messages", treeMessages });
+  });
 
-app.get('/allMessages' , async(req, res) =>{
-    const allMes = await getChatMessages()
-    console.log('first')
-    res.send(allMes)
-}
-)
-}
+  app.get('/get_nodes/:id' , async(req , res) => {
+const getNodes = await getNodeMessages(req.params.id)
+console.log('req.params', req.params)
+res.send(getNodes)
+
+})
+
+};
 
 module.exports = {
-    chatRoutes
-}
+  chatRoutes,
+};

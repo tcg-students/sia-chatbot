c2a4696b122e83f6aa5ed6da2df8a3375c21794a
tumikole <samuelramollo@gmail.com>
2022-06-29 13:03:24 +0200

    added actions for getting the initial tree texts -  frontend/redux/actionCreators

--- frontend/src/Redux/ActionCreators/index.js
+++ frontend/src/Redux/ActionCreators/index.js
@@ -6,7 +6,7 @@ export const getInitialTreeText = (_) => {
     const response = await axios.get("http://localhost:4000/treeMessages");
     const getData = response.data;
     console.log("getData", getData);
-    dispatch({ type: actions.GET_TREE_MESSAGES, payload: getData });
+    dispatch({ type: actions.GETTING_FIRST_TREE_WELCOME_MESSAGES, payload: getData });
   };
 };
 
@@ -15,6 +15,6 @@ export const getInitialNode = (id) => {
     const response = await axios.get(`http://localhost:4000/get_initial_nodes/${id}`);
     const getInitialNodeData = response.data;
     console.log("getInitialNodeData", getInitialNodeData);
-    dispatch({ type: actions.GET_TREE_MESSAGES, payload: getInitialNodeData });
+    dispatch({ type: actions.GETTING_FIRST_NODE_OPTIONS, payload: getInitialNodeData });
   };
 };

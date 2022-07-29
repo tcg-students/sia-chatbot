2bfd9c918041bcfcffd4b78bd75c60349721c660
OkuhleM <lindokuhlea38@gmail.com>
2022-07-29 12:45:31 +0200

    removed a file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./responsive.css";
 import App from "./App";
 import store from "./Redux/Store/index";
 import 'bootstrap/dist/css/bootstrap.min.css';

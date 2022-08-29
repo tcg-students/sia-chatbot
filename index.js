f6560341b995eb8b6d090453d4cef6449edf2f89
tumikole <samuelramollo@gmail.com>
2022-08-29 11:28:05 +0200

    added new code

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const reactAppJson = require("./public/build/asset-manifest.json");
 app.use(express.json());
 app.use(cors());
 
-chatRoutes(app);
-sendGridRoute(app);
+// chatRoutes(app);
+// sendGridRoute(app);
 
 // app.get("/bot", function (req, res) {
 //   res.sendFile(path.join(__dirname, "/public/main.js"));

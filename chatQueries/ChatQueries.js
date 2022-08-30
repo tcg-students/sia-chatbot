0875697a34cc689da51a1d8a4b0920fe64b01c7d
tumikole <samuelramollo@gmail.com>
2022-08-30 14:16:44 +0200

    added connection realese on every query functions

--- chatQueries/ChatQueries.js
+++ chatQueries/ChatQueries.js
@@ -1,31 +1,5 @@
 const { getChats } = require("../database/Chat");
 
-// const sqlQueries = async (statement, parameters) => {
-//   const chats = await getChats();
-//   console.log('chats', chats)
-//   try {
-//     res = await chats.query(statement, parameters);
-//     return res.rows;
-//   } catch (error) {
-//     console.log(error);
-//     client.release();
-//   }
-// };
-
-// const getTcgLogo = async () => {
-//   try {
-//     const connection = await getChats();
-//     let statementQuery =
-//       await connection.query(`SELECT * FROM node WHERE id = ${1};
-//     `);
-//     console.log("statementQuery", statementQuery);
-
-//     return statementQuery;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 const getInitialTreeMessages = async () => {
   const chats = await getChats();
   let query = "SELECT * FROM tree ;";
@@ -33,6 +7,7 @@ const getInitialTreeMessages = async () => {
   console.log('chats',)
   try {
     const res = await chats.query(query);
+    chats.release();
 console.log("res", res)
     return res.rows;
   } catch (error) {
@@ -128,6 +103,7 @@ const getInitialnodes = async (obj) => {
   try {
     let query = `SELECT * FROM node WHERE ${req};`;
     let getInitialNodeQuery = await chats.query(query);
+    chats.release();
 return getInitialNodeQuery
   } catch (error) {
     console.log(error);
@@ -150,11 +126,7 @@ return getInitialNodeQuery
 };
 
 module.exports = {
-  // getTcgLogo,
   getInitialTreeMessages,
   getInitialnodes,
-  // createSingeNode,
-  // createTree,
-  // insertAllNodes,
-  // getNode,
+
 };
